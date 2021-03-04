0865ff646ed369feaa7e17429fea337905986505
Maksim <madmax.space@gmail.com>
2021-03-04 16:42:08 +0600

    Setup DataBinding

--- app/src/main/java/io/maxspace/authmvvm/ui/intro/IntroActivity.java
+++ app/src/main/java/io/maxspace/authmvvm/ui/intro/IntroActivity.java
@@ -1,26 +1,25 @@
 package io.maxspace.authmvvm.ui.intro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import io.maxspace.authmvvm.R;
+import io.maxspace.authmvvm.databinding.ActivityIntroBinding;
 import io.maxspace.authmvvm.ui.intro.adapters.IntroAdapter;
 
 public class IntroActivity extends AppCompatActivity {
-
-    private ViewPager mViewPager;
+    private ActivityIntroBinding mBinding;
     private IntroAdapter mIntroAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_intro);
 
-        mViewPager = findViewById(R.id.walkthroughVp);
         mIntroAdapter = new IntroAdapter(getSupportFragmentManager(), 0);
-
-        mViewPager.setAdapter(mIntroAdapter);
+        mBinding.walkthroughVp.setAdapter(mIntroAdapter);
     }
 }
