1c1b145309a0de84279e1291cc34390d01e5c03b
Maksim <madmax.space@gmail.com>
2021-03-04 12:58:34 +0600

    Initial commit

---
+++ app/src/main/java/io/maxspace/authmvvm/MainActivity.java
@@ -0,0 +1,14 @@
+package io.maxspace.authmvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
